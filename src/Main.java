021e1c585b9774a9f80804bbbbbc7fd7d1b0cf8a
carlosvigan <cfernandezgarcia@hotmail.com>
2022-05-04 20:35:20 +0100

    El primer proyecto

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(){
         System.out.println("Hola");
-        System.out.println("Puta mierda");
+        System.out.println("Puta mierda Carlos");
     }
 }
